dd9046488317f38d142cba0d1b0f90d9fd1169d3
ALKONDR <kondrashkin.98@mail.ru>
2017-04-17 18:07:51 +0300

    basic environment configured

---
+++ blogUI/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+  <h1>That is fucking react project, bitch!</h1>,
+  document.getElementById('root')
+);
---
+++ blogUI/webpack.config.js
@@ -0,0 +1,36 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    stats: 'errors-only',
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'FinJournal',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.ejs'
+    })
+  ]
+}
