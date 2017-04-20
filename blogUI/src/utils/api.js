3294bc492abb22e8a7638d73936db5db6c631544
ALKONDR <kondrashkin.98@mail.ru>
2017-04-20 21:31:57 +0300

    some api logic added

--- blogUI/src/utils/api.js
+++ blogUI/src/utils/api.js
@@ -4,10 +4,11 @@ import axios from 'axios';
 import qs from 'qs';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
-axios.defaults.headers.common.Authorization = window.localStorage.getItem('accessToken') || '';
+axios.defaults.headers.common.Authorization = 'Bearer ' + window.localStorage.getItem('accessToken') || '';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 module.exports = {
+  userLoggedIn: false,
 
   getUsers() {
     return axios.get('/users');
@@ -16,13 +17,52 @@ module.exports = {
   login(username, password) {
     return axios.post('/login', qs.stringify({ username, password }))
                 .then((response) => {
+                  if (response.status !== 200) {
+                    this.userLoggedIn = false;
+                    return false;
+                  }
+
                   window.localStorage.setItem('token', response.data.access.accessToken);
                   window.localStorage.setItem('refreshToken', response.data.refresh);
+                  window.localStorage.setItem('username', username);
+
+                  this.userLoggedIn = true;
+
+                  return true;
                 });
   },
 
+  logout() {
+    window.localStorage.removeItem('token');
+    window.localStorage.removeItem('refreshToken');
+    window.localStorage.removeItem('username');
+  },
+
   signup(email, username, password) {
-    return axios.post('/signup', qs.stringify({ email, username, password }));
+    return axios.post('/signup', qs.stringify({ email, username, password }))
+                .then((response) => {
+                  if (response.status === 200)
+                    return true;
+
+                    return false;
+                });
+  },
+
+  refreshToken() {
+    return axios.get('/refresh',
+                      {
+                        headers: {
+                          'Authorization': 'Bearer ' + window.localStorage.getItem('refreshToken') || ''
+                        }
+                      })
+                .then((response) => {
+                  if (response.status === 200) {
+                    window.localStorage.setItem('token', response.data.accessToken);
+                    return true;
+                  }
+
+                  return false
+                });
   },
 
   getUser(username) {
@@ -33,7 +73,7 @@ module.exports = {
     return axios.get(`users/${username}/stories/${article}`);
   },
 
-  addStory(username, story) {
+  addArticle(username, story) {
     return axios.post(`users/${username}/stories`, story);
   },
 };
