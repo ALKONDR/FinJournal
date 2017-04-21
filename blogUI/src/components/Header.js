c56a9c1fcb19f06ab5199ed258b2f732f7f265fa
ALKONDR <kondrashkin.98@mail.ru>
2017-04-21 12:11:17 +0300

    HeaderState separated

--- blogUI/src/components/Header.js
+++ blogUI/src/components/Header.js
@@ -1,18 +1,14 @@
-import { observable } from 'mobx';
 import React from 'react';
 import NameAndLogo from './NameAndLogo';
 import Search from './Search';
-
-class HeaderState {
-  @observable opened = false;
-}
+import HeaderState from './HeaderState';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="header">
         <NameAndLogo />
-        <Search headerState={new HeaderState()} />
+        <Search headerState={HeaderState} />
       </div>
     );
   }
