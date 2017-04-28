6cb9b123542808dcffda304b2c3ccbe2d1420f8e
ALKONDR <kondrashkin.98@mail.ru>
2017-04-28 13:06:49 +0300

    user avatar in header added

--- blogUI/src/components/Header.js
+++ blogUI/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
 import NameAndLogo from './NameAndLogo';
 import Search from './Search';
 import HeaderState from './HeaderState';
 import LoginButton from './LoginButton';
+import api from '../utils/api';
+import LoginState from './LoginState';
 
+@observer
 class Header extends React.Component {
   render() {
     return (
@@ -13,6 +18,12 @@ class Header extends React.Component {
         <div className="headerLayout">
           <LoginButton loginState={this.props.loginState} />
           <Search headerState={HeaderState} />
+          {LoginState.userLoggedIn ?
+            <Link to={`/users/${api.loggedInUser}`} >
+              <img alt="user" className="authorAvatar" />
+            </Link> :
+            null
+          }
         </div>
       </div>
     );
